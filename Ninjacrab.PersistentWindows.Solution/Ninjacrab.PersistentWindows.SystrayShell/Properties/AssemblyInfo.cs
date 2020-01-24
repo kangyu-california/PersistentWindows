22e9f4cc84c63ca2adef08ab5a69b46bddc01109
kangyu <kangyu.ca@gmail.com>
2020-01-23 22:37:23 -0800

    change program title to PersistentWindows, update version and copyright info

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/Properties/AssemblyInfo.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ninjacrab.PersistentWindows.SystrayShell")]
+[assembly: AssemblyTitle("PersistentWindows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ninjacrab.PersistentWindows.SystrayShell")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("PersistentWindows")]
+[assembly: AssemblyCopyright("Copyright © 2014 - 2020, Min Yong Kim and all other contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0")]
+[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyFileVersion("2.1")]
