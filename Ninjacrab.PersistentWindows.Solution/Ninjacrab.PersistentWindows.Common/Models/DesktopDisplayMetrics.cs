5550a64ba953405e4e1679a2fd24351be1f4b1bf
Kang Yu <kangyu.ca@gmail.com>
2020-09-13 18:04:28 -0700

    simplify monitor data structure

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/Models/DesktopDisplayMetrics.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/Models/DesktopDisplayMetrics.cs
@@ -6,7 +6,7 @@ namespace Ninjacrab.PersistentWindows.Common.Models
 {
     public class DesktopDisplayMetrics
     {
-        private Dictionary<int, Display> monitorResolutions = new Dictionary<int, Display>();
+        private List<Display> monitorResolutions = new List<Display>();
 
         public void AcquireMetrics()
         {
@@ -28,16 +28,10 @@ namespace Ninjacrab.PersistentWindows.Common.Models
                 }
             );
 
-            int displayId = 0;
             foreach (var display in displays)
             {
-                SetMonitor(displayId++, display);
+                monitorResolutions.Add(display);
             }
-        }
-
-        public void SetMonitor(int id, Display display)
-        {
-            monitorResolutions.Add(id, display);
         }
 
         private string BuildKey()
@@ -45,7 +39,7 @@ namespace Ninjacrab.PersistentWindows.Common.Models
             List<string> keySegments = new List<string>();
             foreach (var entry in monitorResolutions)
             {
-                keySegments.Add(string.Format("{0}_Loc{1}x{2}_Res{3}x{4}", entry.Value.DeviceName, entry.Value.Left, entry.Value.Top, entry.Value.ScreenWidth, entry.Value.ScreenHeight));
+                keySegments.Add(string.Format("{0}_Loc{1}x{2}_Res{3}x{4}", entry.DeviceName, entry.Left, entry.Top, entry.ScreenWidth, entry.ScreenHeight));
             }
 
             string key = string.Join("__", keySegments);
