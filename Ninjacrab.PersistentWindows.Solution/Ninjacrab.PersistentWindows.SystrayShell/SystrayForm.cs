6148e7da204c440e050ba737d0c8df100b773744
Kang Yu <kangyu.ca@gmail.com>
2020-11-21 23:16:05 -0800

    tag version 5.6

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/SystrayForm.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/SystrayForm.cs
@@ -206,7 +206,7 @@ namespace Ninjacrab.PersistentWindows.SystrayShell
             if (foundUpgrade)
                 Process.Start($"{Program.ProjectUrl}/releases/latest");
             else
-                Process.Start(Program.ProjectUrl + "/Help.md");
+                Process.Start(Program.ProjectUrl + "/blob/master/Help.md");
         }
 
         private void ExitToolStripMenuItemClickHandler(object sender, EventArgs e)
