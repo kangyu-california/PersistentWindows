c5885a5e6381f4ecd3f32619c196ba11c4339ce0
Kang Yu <kangyu.ca@gmail.com>
2020-11-27 17:11:11 -0800

    remove commented code

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/WinApiBridge/WindowsPosition.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/WinApiBridge/WindowsPosition.cs
@@ -41,25 +41,5 @@ namespace Ninjacrab.PersistentWindows.Common.WinApiBridge
         {
             return string.Format("({0}, {1}), {2} x {3}", Left, Top, Width, Height);
         }
-
-        /*
-        public override bool Equals(object obj)
-        {
-            if (obj is RECT2)
-            {
-                RECT2 ob = (RECT2)obj;
-                if (Left <= -25600 && ob.Left <= -25600)
-                {
-                    // avoid false unequal due to slight value difference 
-                    return true;
-                }
-            }
-            else
-            {
-                return false;
-            }
-            return base.Equals(obj);
-        }
-        */
     }
 }
