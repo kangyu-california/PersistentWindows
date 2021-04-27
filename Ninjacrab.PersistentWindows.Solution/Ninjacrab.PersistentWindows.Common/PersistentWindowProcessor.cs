6dc1c6b55316d868e4172b1905010a6b97a3cac7
Kang Yu <kangyu.ca@gmail.com>
2021-04-27 16:22:14 -0700

    improve runtime of zorder fix

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/PersistentWindowProcessor.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/PersistentWindowProcessor.cs
@@ -27,7 +27,7 @@ namespace Ninjacrab.PersistentWindows.Common
         private const int SlowRestoreLatency = 1000; // delay in milliseconds from power resume to window restore
         private const int MaxRestoreLatency = 2000; // max delay in milliseconds from final restore pass to restore finish
         private const int MinRestoreTimes = 2; // minimum restore passes
-        private const int MaxRestoreTimes = 4; // maximum restore passes
+        private const int MaxRestoreTimes = 3; // maximum restore passes
 
         private const int CaptureLatency = 3000; // delay in milliseconds from window OS move to capture
         private const int UserMoveLatency = 1000; // delay in milliseconds from user move/minimize/unminimize/maximize to capture, must < CaptureLatency
