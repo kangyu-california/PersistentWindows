0aa1d5a3af2fcd3f2558745ceba8aa7198b52722
Kang Yu <kangyu.ca@gmail.com>
2021-06-01 13:57:10 -0700

    speculative fix for issue #117, remove dependency on NLog.dll to avoid crash on 32 bit environment

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/Diagnostics/Log.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.Common/Diagnostics/Log.cs
@@ -2,13 +2,9 @@
 using System.IO;
 using System.Diagnostics;
 
-using NLog;
-using NLog.Config;
-using NLog.Targets;
-
 namespace Ninjacrab.PersistentWindows.Common.Diagnostics
 {
-    public class Log : IDisposable
+    public class Log
     {
         static EventLog eventLog;
         static Log()
@@ -17,17 +13,6 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
             //eventLog.Source = System.Windows.Forms.Application.ProductName;
             eventLog.Source = "Application";
 
-            var config = new LoggingConfiguration();
-
-            // Step 2. Create targets and add them to the configuration 
-            var consoleTarget = new ColoredConsoleTarget();
-            config.AddTarget("console", consoleTarget);
-
-            var fileTarget = new FileTarget();
-            config.AddTarget("file", fileTarget);
-
-            // Step 3. Set target properties 
-            consoleTarget.Layout = @"${date:format=HH\\:mm\\:ss} ${logger} ${message}";
 #if DEBUG
             //string fileName = "${basedir}/PersistentWindows.Log";
             string fileName = "PersistentWindows.Log";
@@ -36,54 +21,17 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
             string fileName = $"{tempFolderPath}/PersistentWindows.Log";
 #endif
             File.Delete(fileName);
-            fileTarget.FileName = fileName;
-
-            fileTarget.Layout = "${date:format=HH\\:mm\\:ss} ${logger} ${message}";
-
-            // Step 4. Define rules
-            var rule1 = new LoggingRule("*", LogLevel.Trace, consoleTarget);
-            config.LoggingRules.Add(rule1);
-
-            var rule2 = new LoggingRule("*", LogLevel.Trace, fileTarget);
-            config.LoggingRules.Add(rule2);
-
-            // Step 5. Activate the configuration
-            LogManager.Configuration = config;
         }
 
         /// <summary>
         /// Occurs when something is logged. STATIC EVENT!
         /// </summary>
-        public static event Action<LogLevel, string> LogEvent;
-
-        private static Logger _logger; 
-        private static Logger Logger
-        {
-            get
-            {
-                if(_logger == null)
-                {
-                    _logger = LogManager.GetLogger("Logger");
-                }
-                return _logger;
-            }
-        }
-
-        private static void RaiseLogEvent(LogLevel level, string message)
-        {
-            // could, should, would write a new logging target but this is brute force faster
-            if(LogEvent != null)
-            {
-                LogEvent(level, message);
-            }
-        }
 
         public static void Trace(string format, params object[] args)
         {
 #if DEBUG
             var message = Format(format, args);
-            Logger.Trace(message);
-            RaiseLogEvent(LogLevel.Trace, message);
+            Console.Write(message);
 #endif
         }
 
@@ -91,8 +39,7 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
         {
 #if DEBUG
             var message = Format(format, args);
-            Logger.Info(message);
-            RaiseLogEvent(LogLevel.Info, message);
+            Console.Write(message);
 #endif
         }
 
@@ -111,8 +58,7 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
                 return;
             }
 #if DEBUG
-            Logger.Error(message);
-            RaiseLogEvent(LogLevel.Error, message);
+            Console.Write(message);
 #endif
  
             eventLog.WriteEntry(System.Windows.Forms.Application.ProductName + ": " + message, EventLogEntryType.Information, 9999, 0);
@@ -121,7 +67,8 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
         public static void Event(string format, params object[] args)
         {
             var message = Format(format, args);
-            Logger.Trace(message);
+            Console.Write(message);
+            message = message.Substring(message.IndexOf("::") + 3);
             eventLog.WriteEntry(System.Windows.Forms.Application.ProductName + ": " + message, EventLogEntryType.Information, 9990, 0);
         }
 
@@ -133,12 +80,8 @@ namespace Ninjacrab.PersistentWindows.Common.Diagnostics
         /// <returns></returns>
         private static string Format(string format, params object[] args)
         {
-            return args == null || args.Length == 0 ? format : string.Format(format, args);
+            return string.Format($"{DateTime.Now} :: {format}\n", args);
         }
 
-        public void Dispose()
-        {
-            eventLog.Dispose();
-        }
     }
 }
