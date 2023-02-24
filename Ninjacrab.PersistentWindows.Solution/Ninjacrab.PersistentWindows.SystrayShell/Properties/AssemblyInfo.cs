816b0558f52a891fe43e2629ed524d38189d8f34
Kang Yu <kangyu.ca@gmail.com>
2023-02-23 20:02:48 -0800

    tag 5.41

--- Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/Properties/AssemblyInfo.cs
+++ Ninjacrab.PersistentWindows.Solution/Ninjacrab.PersistentWindows.SystrayShell/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PersistentWindows")]
-[assembly: AssemblyCopyright("Copyright © 2014 - 2022, PersistentWindows")]
+[assembly: AssemblyCopyright("Copyright © 2014 - 2023, PersistentWindows")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.40.*")]
+[assembly: AssemblyVersion("5.41.*")]
 
