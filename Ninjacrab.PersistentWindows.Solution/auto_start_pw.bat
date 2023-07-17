dfb76a8270676f90603f2c4dfcbdf57ec342fa59
Kang Yu <kangyu.ca@gmail.com>
2023-07-17 10:17:25 -0700

    add script to create auto start task

---
+++ Ninjacrab.PersistentWindows.Solution/auto_start_pw.bat
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy Bypass -File auto_start_pw_aux.ps1
+pause
---
+++ Ninjacrab.PersistentWindows.Solution/auto_start_pw_aux.ps1
@@ -0,0 +1,35 @@
+## Replace with your desired command arguments
+$arguments = "-delay_start 10"  
+
+## rename the task as you like
+$taskName = "StartPersistentWindows" + $env:username
+$taskDescription = "This task starts automatically when " + $env:username + " login."
+
+$existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+
+if ($existingTask -ne $null) {
+    Write-Host "Remove existing task."
+	Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+}
+
+$executablePath = $PSScriptRoot + "\PersistentWindows.exe"
+
+$action = New-ScheduledTaskAction -Execute $executablePath
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:username
+
+## set PW process priority to below normal
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Priority 8
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Description $taskDescription
+
+$task = Get-ScheduledTask -TaskName $taskName
+$taskSettings = $task.Settings
+$taskSettings.ExecutionTimeLimit = "PT0S" # disable time limit
+Set-ScheduledTask -TaskName $taskName -Settings $taskSettings
+
+$task.Actions[0].Arguments = $arguments
+Set-ScheduledTask -TaskName $taskName -TaskPath $task.TaskPath -Action $task.Actions
+
+## Set the task to run with highest privileges
+$principal = New-ScheduledTaskPrincipal -UserId $env:username  -RunLevel Highest
+$task.Principal = $principal
+Set-ScheduledTask -TaskName $taskName -TaskPath $task.TaskPath -Principal $principal
