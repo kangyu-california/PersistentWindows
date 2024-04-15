cb334676c0c5e4cab45e5a1210db859a1e213cd6
Kang Yu <kangyu.ca@gmail.com>
2024-04-15 12:32:19 -0700

    support 360ChromeX browser

--- Ninjacrab.PersistentWindows.Solution/Common/PersistentWindowProcessor.cs
+++ Ninjacrab.PersistentWindows.Solution/Common/PersistentWindowProcessor.cs
@@ -128,7 +128,7 @@ namespace PersistentWindows.Common
 
         private static HashSet<string> browserProcessNames = new HashSet<string>()
         {
-            "chrome", "firefox", "msedge", "vivaldi", "opera", "brave"
+            "chrome", "firefox", "msedge", "vivaldi", "opera", "brave", "360ChromeX"
         };
 
         private HashSet<string> ignoreProcess = new HashSet<string>();
