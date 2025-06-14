b7deb721677efeeb32160053503a960f6a81603d
Alex Strobel <dev@bitkleber.de>
2025-06-14 12:35:43 +0200

    Arranged and anchored the controls of Layout selection window
    When resizing the window the controls make use of the available space now.
    Also added a cancel button and set the OkButton and CancelButton properties so
    that this dialog can be quit using the keyboard (by pressing Enter/Return or
    Escape key).

--- Ninjacrab.PersistentWindows.Solution/Common/DbKeySelect.Designer.cs
+++ Ninjacrab.PersistentWindows.Solution/Common/DbKeySelect.Designer.cs
@@ -32,22 +32,27 @@ namespace PersistentWindows.Common
             this.ListLayout = new System.Windows.Forms.ListBox();
             this.Ok = new System.Windows.Forms.Button();
             this.selected = new System.Windows.Forms.Label();
+            this.Cancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // ListLayout
+            // LayoutList
             // 
+            this.ListLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.ListLayout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ListLayout.FormattingEnabled = true;
-            this.ListLayout.ItemHeight = 20;
-            this.ListLayout.Location = new System.Drawing.Point(60, 30);
-            this.ListLayout.Name = "ListLayout";
-            this.ListLayout.Size = new System.Drawing.Size(685, 244);
+            this.ListLayout.ItemHeight = 17;
+            this.ListLayout.Location = new System.Drawing.Point(12, 12);
+            this.ListLayout.Name = "LayoutList";
+            this.ListLayout.Size = new System.Drawing.Size(776, 293);
             this.ListLayout.TabIndex = 0;
             this.ListLayout.SelectedIndexChanged += new System.EventHandler(this.ListLayout_SelectedIndexChanged);
             // 
             // Ok
             // 
-            this.Ok.Location = new System.Drawing.Point(351, 385);
+            this.Ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Ok.Location = new System.Drawing.Point(572, 402);
             this.Ok.Name = "Ok";
             this.Ok.Size = new System.Drawing.Size(105, 36);
             this.Ok.TabIndex = 1;
@@ -55,17 +60,35 @@ namespace PersistentWindows.Common
             this.Ok.UseVisualStyleBackColor = true;
             this.Ok.Click += new System.EventHandler(this.Ok_Click);
             // 
-            // selected
+            // LayoutItemSelected
             // 
-            this.selected.Location = new System.Drawing.Point(60, 290);
-            this.selected.Name = "selected";
-            this.selected.Size = new System.Drawing.Size(683, 80);
+            this.selected.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selected.Location = new System.Drawing.Point(12, 311);
+            this.selected.Margin = new System.Windows.Forms.Padding(3);
+            this.selected.Name = "LayoutItemSelected";
+            this.selected.Size = new System.Drawing.Size(776, 85);
             this.selected.TabIndex = 2;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Location = new System.Drawing.Point(683, 402);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(105, 36);
+            this.Cancel.TabIndex = 3;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
             // 
             // DbKeySelect
             // 
+            this.AcceptButton = this.Ok;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.CancelButton = this.Cancel;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cancel);
             this.Controls.Add(this.selected);
             this.Controls.Add(this.Ok);
             this.Controls.Add(this.ListLayout);
@@ -81,5 +104,6 @@ namespace PersistentWindows.Common
         private System.Windows.Forms.ListBox ListLayout;
         private System.Windows.Forms.Button Ok;
         private System.Windows.Forms.Label selected;
+        private System.Windows.Forms.Button Cancel;
     }
 }
